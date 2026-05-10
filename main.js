1ad6014460b9a7eb6bd569fa449b3c1f285511a8
Aman Arazmedov <aman@example.com>
2026-05-10 13:38:49 +0300

    modules

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 "use strict";
 
-import { fetchAndRenderComments } from "./modules/api.js";
 import { commentsElement } from "./modules/variables.js";
 import { initAddCommentButton } from "./modules/listeners.js";
+import { fetchAndRenderComments } from "./modules/api.js";
 
 commentsElement.innerHTML = "<li>Комментарии загружаются...</li>";
 commentsElement.style.display = "flex";
